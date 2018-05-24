811d83094a2307178855838b7e48bfc4265fb87c
Łukasz Rutkowski <lukus178@gmail.com>
2018-05-24 14:12:36 +0200

    Connect seekbar and progress input field

--- app/src/main/java/me/thanel/readtracker/MainActivity.kt
+++ app/src/main/java/me/thanel/readtracker/MainActivity.kt
@@ -2,6 +2,11 @@ package me.thanel.readtracker
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import android.widget.SeekBar
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.item_book_card.*
 import kotlinx.coroutines.experimental.CommonPool
 import kotlinx.coroutines.experimental.android.UI
@@ -15,6 +20,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        progressInput.filters = arrayOf(RangeInputFilter(0, 100))
+        mergeSeekBarAndInput(progressSeekBar, progressInput)
+
         launch(UI) {
             val response = withContext(CommonPool) {
                 GoodreadsApi.service.listReviews(77741768, "currently-reading").await()
@@ -27,5 +35,33 @@ class MainActivity : AppCompatActivity() {
                 bookAuthorView.text = review.book.authors.joinToString(prefix = "by ") { it.name }
             }
         }
+    }
+
+    private fun mergeSeekBarAndInput(seekBar: SeekBar, editText: EditText) {
+        var skipChange = false
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                if (skipChange) return
+                val percentage = ((progress / seekBar.max.toFloat()) * 100).toInt()
+                editText.setText(percentage.toString())
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) = Unit
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) = Unit
+        })
+        editText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                skipChange = true
+                val input = s?.toString()?.toIntOrNull() ?: 0
+                seekBar.progress = ((input / 100f) * seekBar.max).toInt()
+                skipChange = false
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) =
+                Unit
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) = Unit
+        })
     }
 }
