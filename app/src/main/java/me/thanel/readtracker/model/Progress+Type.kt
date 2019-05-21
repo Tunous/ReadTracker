3d05e996f6f4e57248de36cd9a7075dcb43f1e26
Łukasz Rutkowski <lukus178@gmail.com>
2019-05-21 20:12:37 +0200

    Fix actual page calculation

--- app/src/main/java/me/thanel/readtracker/model/Progress+Type.kt
+++ app/src/main/java/me/thanel/readtracker/model/Progress+Type.kt
@@ -4,7 +4,11 @@ import me.thanel.readtracker.SelectWithBookInformation
 import kotlin.math.roundToInt
 
 val SelectWithBookInformation.actualPage: Int
-    get() = page ?: percentToPage(percent ?: 0, numPages)
+    get() {
+        val page = page
+        if (page != null && page > 0) return page
+        return percentToPage(percent ?: 0, numPages)
+    }
 
 private fun percentToPage(percent: Int, numPages: Int): Int {
     val floatPercent = percent / 100f
